7b6d618a09eaff9e58b3b05e0b3f8294a45515c0
youssefoesmail <youssefo.esmail@gmail.com>
2023-11-14 20:30:23 +0300

    fFinish

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const play = document.querySelector(".play");
 const start = document.querySelector(".start")
 play.addEventListener("click", () => {
   start.style.display = "none"
+  mainDiv.style.display = "block"
   startTheGame()
 });
 
@@ -12,55 +13,77 @@ const hangManQu = [
     {
         que : "commonly referred to as the domestic pet or house pet",
         ans : "catc",
-        space : "_ _ _"//should be array
     },
 ]
 
 const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
 
-const images = ["PIC/hangman_pic_1.png"]
+const images = ["PIC/hangman_pic_1.png","PIC/hangman_pic_2.png","PIC/hangman_pic_3.png","PIC/hangman_pic_4.png","PIC/hangman_pic_5.png","PIC/hangman_pic_6.png","PIC/hangman_pic_7.png"]
 
 const ansSpace = []
+
+const mainDiv = document.createElement("div")
+mainDiv.classList.add("main_2")
+const question = document.createElement("div")
+const pics = document.createElement("img")
+const alpha = document.createElement("div")
+const divSpace = document.createElement("div")
 
 const startTheGame = () =>
 {
   hangManQu.forEach((q , i) => {
-    const mainDiv = document.createElement("div")
-    mainDiv.classList.add("main_2")
-    const question = document.createElement("div")
-    const pics = document.createElement("img")
-    const alpha = document.createElement("div")
-    const divSpace = document.createElement("div")
 
-    for (const key in q.ans)
+    for(let i = 0 ; i < q.ans.length ; i++)
     {
-      console.log(q.ans[key])
+      ansSpace.push("_")
     }
-
+    
     alphabet.forEach((char , i)=>{
       const btn = document.createElement("button")
       btn.innerText = char
       alpha.append(btn)
       btn.addEventListener("click",(e)=>{
-      for (const key in q.ans)
-      {
-        if(q.ans[key] === e.target.innerText)
+        if(q.ans.includes(e.target.innerText))
         {
-          console.log(key)//q.ans.lastIndexOf(e.target.innerText))
+          for(const key in q.ans) 
+          {
+            if(q.ans[key] === e.target.innerText)
+            {
+              ansSpace.splice(key, 1, q.ans[key]);
+              divSpace.innerText = ansSpace.join(" ")
+            }
+          }
         }
-      }
+        else
+        {
+          console.log("f")
+          e.target.style.display = "none"
+          images.shift()
+          if(images[0] === undefined)
+          {
+            restart()
+          }
+          else
+          {
+            pics.src = images[0]
+          }
+        }
       })
     })
 
     question.innerText = q.que
-    divSpace.innerText = q.space
+    divSpace.innerText = ansSpace.join(" ")
     pics.src = images[0]
     body.append(mainDiv)
     mainDiv.append(pics,question,alpha)
     question.append(divSpace)
-    console.log(q)
   })
 }
 
+const restart = () =>
+{
+  mainDiv.style.display = "none"
+  start.style.display = "block"
+}
 
 
