69891d5097adf22c0e3e2412da4cfb02fd9011c5
youssefoesmail <youssefo.esmail@gmail.com>
2023-11-15 23:37:44 +0300

    finishing

--- main.js
+++ main.js
@@ -1,22 +1,38 @@
 
 const play = document.querySelector(".play");
+const btnque = document.querySelector(".question")
 const start = document.querySelector(".start")
+const main_1 = document.querySelector(".main_1")
 play.addEventListener("click", () => {
   start.style.display = "none"
   mainDiv.style.display = "block"
   startTheGame()
 });
 
+btnque.addEventListener("click", () => {
+  start.style.display = "none"
+  mainDiv.style.display = "none"
+  crearteQuestions()
+});
+
 const body = document.querySelector("body")
 
 const hangManQu = [
   {
-      que : "commonly referred to as the domestic pet or house pet",
-      ans : "cat",
+    que : "commonly referred to as the domestic pet or house pet",
+    ans : "cat",
   },
   {
     que : "An animal use all four limbs—two arms and two legs—to walk and run. Their hands and feet are good for grasping tree branches",
     ans : "monkey",
+  },
+  {
+    que : "A pet animal has sharp teeth so that it can eat flesh very easily, it has four legs, sometimes used for hunting",
+    ans : "dog"
+  },
+  {
+    que : "the largest animal on earth and have distinctly massive bodies, large ears, and long trunks. They use their trunks to pick up objects, trumpet warnings",
+    ans : "elephant"
   },
 ]
 
@@ -28,6 +44,8 @@ const ansSpace = []
 
 const mainDiv = document.createElement("div")
 mainDiv.classList.add("main_2")
+const timerDiv = document.createElement("div")
+timerDiv.classList.add("timer")
 const question = document.createElement("div")
 const pics = document.createElement("img")
 const alpha = document.createElement("div")
@@ -39,11 +57,82 @@ const btn_2 = document.createElement("button")
 btn_2.innerText = "Quit"
 btn_1.style.alignItems = "center"
 
+// create more que's from the user
+const styleDiv = document.createElement("div")
+styleDiv.classList.add("q_div_layout")
+const queDiv = document.createElement("div")
+queDiv.innerText = "question"
+queDiv.classList.add("que_div")
+const queInput = document.createElement("textarea")
+queInput.rows = 10
+queInput.classList.add("que_input_div")
+const ansDiv = document.createElement("div")
+ansDiv.innerText = "answer"
+ansDiv.classList.add("ans_div")
+const ansInput = document.createElement("input")
+ansInput.classList.add("ans_input_div")
+const btnInput = document.createElement("button")
+btnInput.innerText = "Insert the question"
+btnInput.classList.add("btn_input_div")
+const btnBack = document.createElement("button")
+btnBack.innerText = "Back"
+btnBack.classList.add("btn_back_div")
+//const btnInputLayout = document.createElement("button")
+
+// question function
+const crearteQuestions = () =>
+{
+  // insert elements
+  main_1.append(styleDiv)
+  styleDiv.append(queDiv,queInput,ansDiv,ansInput,btnInput,btnBack)
+  styleDiv.style.display = "block"
+}
+// insert button
+btnInput.addEventListener("click" , ()=>
+{
+  insertF()
+})
+
+// back to main
+btnBack.addEventListener("click" , ()=>
+{
+  styleDiv.style.display = "none"
+  restart()
+})
+
+// insert function
+const insertF = () =>
+{
+  hangManQu.push({que:queInput.value , ans:ansInput.value}) 
+  console.log(hangManQu)
+}
+
+
 const getRandomInt = (max) => {
   return Math.floor(Math.random() * max);
 }
 
 let q = hangManQu[getRandomInt(hangManQu.length)]
+
+let min = 60
+
+const timeOut = () =>
+{
+  if(min > 0)
+  {
+    min = min -1
+    timerDiv.innerText = min
+  }
+  if(min === 0)
+  {
+    timerDiv.innerText = min
+    pics.src = images[images.length - 1]
+    document.querySelector(".alphaChar").innerText = ""
+    finish()
+  }
+}
+
+let t
 
 const startTheGame = () =>
 {
@@ -51,7 +140,9 @@ const startTheGame = () =>
     {
       ansSpace.push("_")
     }
-    
+
+    t = setInterval(timeOut, 1000)
+
     alphabet.forEach((char , i)=>{
       const btn = document.createElement("button")
       btn.innerText = char
@@ -93,13 +184,14 @@ const startTheGame = () =>
     divSpace.innerText = ansSpace.join(" ")
     pics.src = images[0]
     body.append(mainDiv)
-    mainDiv.append(pics,question,alpha)
+    mainDiv.append(timerDiv,pics,question,alpha)
     question.append(divSpace)
   })
 }
 
 const finish = () =>
 {
+  clearInterval(t)
   btn_1.style.display = "block"
   btn_2.style.display = "block"
   console.log(divSpace.innerText)
@@ -120,6 +212,8 @@ const finish = () =>
 
 const restart = () =>
 {
+  clearInterval(t)
+  min = 60
   mainDiv.style.display = "none"
   start.style.display = "block"
   images = ["PIC/hangman_pic_1.png","PIC/hangman_pic_2.png","PIC/hangman_pic_3.png","PIC/hangman_pic_4.png","PIC/hangman_pic_5.png","PIC/hangman_pic_6.png","PIC/hangman_pic_7.png"]
@@ -135,6 +229,8 @@ const restart = () =>
 
 const palyAgain = () =>
 {
+  clearInterval(t)
+  min = 60
   images = ["PIC/hangman_pic_1.png","PIC/hangman_pic_2.png","PIC/hangman_pic_3.png","PIC/hangman_pic_4.png","PIC/hangman_pic_5.png","PIC/hangman_pic_6.png","PIC/hangman_pic_7.png"]
   for(let i = 0 ; i<q.ans.length ; i++)
   {
